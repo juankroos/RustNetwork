41bde6ff959c42c385d29e350c991f79c86f0298
juankroos <58156316+juankroos@users.noreply.github.com>
2025-05-22 10:15:49 +0100

    blabla

--- project/src/sever.rs
+++ project/src/sever.rs
@@ -1,10 +1,12 @@
-use std::net::TcpListener;
+use std::net::{TcpListener,TcpStream};
 
+fn handle_client(stream: TcpStream) {
+    
+}
 fn main() {
-    let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
-
-    for stream in listener.incoming() {
-        let stream = stream.unwrap();
-        println!("Connection established!");
+    let listener = TcpListener::bind("127.0.0.1:80")?;
+    for stream in listener.incoming(){
+        handle_client(stream?);
     }
+    ok(())
 }
