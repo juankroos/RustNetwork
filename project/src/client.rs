de1ab81adb9077a3f29ae09799ce6ce8671ddafe
juankroos <58156316+juankroos@users.noreply.github.com>
2025-05-22 11:56:27 +0100

    blablaa

--- project/src/client.rs
+++ project/src/client.rs
@@ -8,14 +8,20 @@ struct client{
     message: String,
     server_ip: Int ,
     status: String,
+}
+
+impl Client{
 
     fn connection(&self) -> std::io::Result<()> {
-        let mut stream = TcpStream::connect(self.address)?;
-        stream.write(&[1]?);
-        stream.read(&mut [0; 128])?;
-        Ok(())
-        println!("{} is connected to {}", self.name, self.address);
+        match Tcpstream::connect(self.address){
+            Ok(stream) => println!("{} is connected to {}", self.name, self.address),
+            Err(e) => {
+                println!("Failed to connect: {}", e);
+                return Err(e);
+            }
+        }
     }
+
     fn disconnect(&self) {
         println!("{} is disconnected from {}", self.name, self.address);
     }
@@ -23,4 +29,9 @@ struct client{
     fn send_message(&self, message: &str) {
         println!("{} sent: {}", self.name, message);
     }
-}
+    }
+    fn send_message(&self, message: &str) {
+        let stream = TcpStream::connect(self.address).unwrap();
+        stream.write_all(message.as_bytes()).unwrap();
+        println!("{} sent: {}", self.name, message);
+    }
