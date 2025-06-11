8195583fb0a091bde6d37b57e937c3da4145379b
juankroos <58156316+juankroos@users.noreply.github.com>
2025-06-11 09:46:08 +0100

    commit ahh

--- project/src/Websocket/client.rs
+++ project/src/Websocket/client.rs
@@ -1,5 +1,5 @@
 
-
+//server websockets
 use tokio::net::TcpListener;
 use tokio_tungstenite::accept_async;
 use tokio_tungstenite::tungstenite::protocol::Message;
@@ -35,63 +35,10 @@ async fn handle_connection(stream: tokio::net::TcpStream) -> Result<()> {
     Ok(())
 }
 
-use tokio_tungstenite::connect_async;
-use tokio_tungstenite::tungstenite::protocol::Message;
-use anyhow::Result;
-use futures_util::{SinkExt, StreamExt};
-use url::Url;
-
-#[tokio::main]
-async fn main() -> Result<()> {
-    let url = Url::parse("ws://127.0.0.1:8080")?;
-    let (mut ws_stream, _) = connect_async(url.as_str()).await.expect("Failed to connect");
-    println!("WebSocket client connected");
-
-    // Sending a message to the server
-    let message = "Hello, Server!";
-    ws_stream.send(Message::Text(message.into())).await?;
-
-    // Receiving messages from the server
-    while let Some(msg) = ws_stream.next().await {
-        match msg? {
-            Message::Text(text) => {
-                println!("Received message from server: {}", text);
-            }
-            _ => {}
-        }
-    }
-
-    Ok(())
-}
-
-use tokio_tungstenite::connect_async;
-use tokio_tungstenite::tungstenite::protocol::Message;
-use anyhow::Result;
-use futures_util::{SinkExt, StreamExt};
-use url::Url;
-
-#[tokio::main]
-async fn main() -> Result<()> {
-    let url = Url::parse("ws://127.0.0.1:8080")?;
-    let (mut ws_stream, _) = connect_async(url.as_str()).await.expect("Failed to connect");
-    println!("WebSocket client connected");
-
-    // Sending a message to the server
-    let message = "Hello, Server!";
-    ws_stream.send(Message::Text(message.into())).await?;
-
-    // Receiving messages from the server
-    while let Some(msg) = ws_stream.next().await {
-        match msg? {
-            Message::Text(text) => {
-                println!("Received message from server: {}", text);
-            }
-            _ => {}
-        }
-    }
-
-    Ok(())
-}
+///
+/// 
+/// 
+/// websocket client
 use tokio_tungstenite::connect_async;
 use tokio_tungstenite::tungstenite::protocol::Message;
 use anyhow::Result;
