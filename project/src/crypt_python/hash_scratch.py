3f51e25c3d3967bbc14d2eb4da8d3f6787e93b1f
juankroos <58156316+juankroos@users.noreply.github.com>
2025-06-25 17:07:30 +0100

    update commit

--- project/src/crypt_python/hash_scratch.py
+++ project/src/crypt_python/hash_scratch.py
@@ -2,8 +2,14 @@ import hashlib
 
 h = hashlib.sha256()
 h.update(b"Hello World")
-print(h.hexdigest())
+h = hashlib.shake_256(b"Hello World")
+
+print(h.hexdigest(20))
+print('\n')
+print(h.hexdigest(100))
+
 print('hehehehehe')
+
 h1 = hashlib.new('sha256')
 h1.update(b"Hello World")
 print(h1.hexdigest())
