ddf3bac8a2695cf1fa8118ae2285d37b82dc43f6
juankroos <58156316+juankroos@users.noreply.github.com>
2025-06-26 13:13:11 +0100

    update commit

--- project/src/crypt_python/hash_scratch.py
+++ project/src/crypt_python/hash_scratch.py
@@ -1,4 +1,6 @@
 import hashlib
+from hashlib import pbkdf2_hmac as pb
+# from hashlib import sha256, shake_256
 
 h = hashlib.sha256()
 h.update(b"Hello World")
@@ -8,8 +10,13 @@ print(h.hexdigest(20))
 print('\n')
 print(h.hexdigest(100))
 
+
 print('hehehehehe')
 
 h1 = hashlib.new('sha256')
 h1.update(b"Hello World")
 print(h1.hexdigest())
+
+our_app_iters = 500_000
+dk = pb('sha256', b'password', b'salt' * 2, our_app_iters)
+dk.hex()
