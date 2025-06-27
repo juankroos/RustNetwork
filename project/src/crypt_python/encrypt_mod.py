3ae5062465e5624bb3cc00a4a80e8091d90825f5
juankroos <58156316+juankroos@users.noreply.github.com>
2025-06-27 15:20:25 +0100

    update commit

--- project/src/crypt_python/encrypt_mod.py
+++ project/src/crypt_python/encrypt_mod.py
@@ -22,8 +22,8 @@ with open(r'E:\RustNetwork\project\src\crypt_python\nba_encrypted.csv', 'wb') as
     f.write(encrypted)
 
 #simple encryption too simple 
-def enc_file():
-    input1 = "juan"
+def enc_file(input1):
+    #input1 = "juan"
     lib = 'abcdefghijklmnopqrstuvwxyz'
     new_input = np.array([])
     key = 2
@@ -31,14 +31,39 @@ def enc_file():
     for i in input1:
         for j in lib:
             if i == j:
-                index = lib.index(j) + key % len(lib)
+                index = (lib.index(j) + key) % len(lib)
                 value = lib[index]
                 new_input = np.append(value, new_input)
-                print(f"the index of {i} is {index} and the count is {lib.index(j)}...blabla {value}")
+                #print(f"the index of {i} is {index} and the count is {lib.index(j)} ...blabla {value}")
                 #count +=0
-    #a = ' '.join(new_input)
-    print(f"the new input is {a}")
+    a = ''.join(new_input)
+    #print(f"the new input is {a} and the first was {input1}")
+    return a
+
+def decr_file(input1):
+    lib = 'abcdefghijklmnopqrstuvwxyz'
+    new_input = np.array([])
+    key = 5
+    #count = 0
+    for i in input1:
+        for j in lib:
+            if i == j:
+                index = (lib.index(j) - key) % len(lib)
+                value = lib[index]
+                new_input = np.append(value, new_input)
+                #print(f"the index of {i} is {index} and the count is {lib.index(j)} ...blabla {value}")
+                #count +=0
+    a = ''.join(new_input)
+    #print(f"the new input is {a} and the first was {input1}")
+    return a
+
+
 
 if __name__ == "__main__":
-    enc_file()
+    #some = input("Enter a string to encrypt: ")
+    some = 'hello ahhh'
+    res = enc_file(some)
+    #print (res)
+    req = decr_file(res)
+    print(req)
    
