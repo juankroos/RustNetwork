af8d20cf8102a7149885aac09c70597464ad9f72
juankroos <58156316+juankroos@users.noreply.github.com>
2025-08-18 13:27:01 +0100

    froms ASL

--- mon-projet/src/index.js
+++ mon-projet/src/index.js
@@ -1,4 +1,12 @@
 import React from 'react';
+import { createRoot } from 'react-dom/client';
+import App from './App';
+
+const root = createRoot(document.getElementById('root'));
+root.render(<App />);
+
+/*
+import React from 'react';
 import ReactDOM from 'react-dom/client';
 //import ReactDOM from 'react-dom';
 import './index.css';
@@ -18,13 +26,5 @@ root.render(
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
 
+*/
 
-
-
-
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
